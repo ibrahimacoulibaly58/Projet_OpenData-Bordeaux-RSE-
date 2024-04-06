bc4d69676ec870cab9a21837f896498eb10e3997
l.tripied <lt@bziiit.com>
2024-04-06 23:41:25 +0200

    V2-Liste_MAP_Stat_23

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from organisations_engagees import display_organisations_engagees
 from localisation import display_map
 from statistiques import main as display_statistics
 
+
 # Main function orchestrating the app UI
 def main():
     st.sidebar.title("Open Data RSE Bordeaux Métropole")
@@ -15,11 +16,13 @@ def main():
     elif app_mode == "Statistiques":
         display_statistics()
 
+
   # Après toutes les autres instructions dans votre barre latérale :
     st.sidebar.markdown("---")  # Ajoute une ligne de séparation visuelle
     st.sidebar.markdown("Powered by **bziiit IA RSE**")
     st.sidebar.markdown("---")  # Ajoute une ligne de séparation visuelle
     st.sidebar.markdown("2024 : Open source en Licence MIT")
+
 
 if __name__ == "__main__":
     main()
