7923d70d6f873305ec9faea485d3538e67508c85
l.tripied <lt@bziiit.com>
2024-04-06 15:14:26 +0200

    Pytorch V0.23

--- organisations_engagees.py
+++ organisations_engagees.py
@@ -1,20 +1,28 @@
-import streamlit as st
-import pandas as pd
+import requests
+import folium
+from streamlit_folium import folium_static
 
-def display_organisations_engagees(data):
-    st.markdown("## OPEN DATA RSE")
-    st.markdown("### Découvrez les organisations engagées RSE de la métropole de Bordeaux")
-    num_etablissements = len(data)
-    
-    if num_etablissements > 0:
-        st.markdown(f"Nombre d'établissements : {num_etablissements}")
-        df = pd.DataFrame(data)
-        st.dataframe(df[['nom_courant_denomination', 'commune', 'libelle_section_naf', 'tranche_effectif_entreprise', 'action_rse']].rename(columns={
-            'nom_courant_denomination': 'Nom',
-            'commune': 'Commune',
-            'libelle_section_naf': 'Section NAF',
-            'tranche_effectif_entreprise': 'Effectif',
-            'action_rse': 'Action RSE'
-        }))
+def get_data():
+    url = "https://opendata.bordeaux-metropole.fr/api/records/1.0/search/?dataset=met_etablissement_rse&q=&rows=100"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        records = data.get("records", [])
+        return [record.get("fields") for record in records]
     else:
-        st.error("Données OPEN DATA RSE Bordeaux non disponibles actuellement. Veuillez vous reconnecter ultérieurement.")
+        return []
+
+def display_map(data):
+    m = folium.Map(location=[44.837789, -0.57918], zoom_start=12)
+    for item in data:
+        point_geo = item.get('point_geo')
+        if isinstance(point_geo, dict):
+            lon = point_geo.get('lon')
+            lat = point_geo.get('lat')
+            if lon and lat:
+                folium.Marker(
+                    [lat, lon],
+                    icon=folium.Icon(color="green", icon="leaf"),
+                    popup=item.get('nom_courant_denomination', 'Information non disponible'),
+                ).add_to(m)
+    folium_static(m)
