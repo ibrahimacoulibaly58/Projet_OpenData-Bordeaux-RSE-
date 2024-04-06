8f88dc692a58cdcd6785f686010870c8e96bd8f6
l.tripied <lt@bziiit.com>
2024-04-06 20:24:36 +0200

    Pytorch v.41

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import requests  # Ajoutez cette ligne au début de votre fichier
 import streamlit as st
 from organisations_engagees import display_organisations_engagees
 from localisation import display_map
@@ -6,18 +7,24 @@ def get_data():
     url = "https://opendata.bordeaux-metropole.fr/api/records/1.0/search/?dataset=met_etablissement_rse&q=&rows=100"
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Cela va déclencher une exception pour les réponses non-200
-        data = response.json()
-        records = data.get("records", [])
-        if records:
-            return [record.get("fields") for record in records]
+        if response.status_code == 200:
+            data = response.json()
+            records = data.get("records", [])
+            cleaned_data = []
+            for record in records:
+                fields = record.get("fields", {})
+                geoloc = fields.get("geolocalisation")
+                if geoloc and isinstance(geoloc, list) and len(geoloc) == 2:
+                    lat, lon = geoloc
+                    cleaned_data.append({"lat": lat, "lon": lon, "name": fields.get("nom_courant_denomination", "Inconnu")})
+            return cleaned_data
         else:
-            st.error("Aucun enregistrement trouvé dans les données de l'API.")
+            st.error(f"Failed to fetch data. Status code: {response.status_code}")
             return []
     except requests.RequestException as e:
-        st.error(f"Erreur lors de la récupération des données de l'API: {e}")
+        st.error(f"Error occurred: {e}")
         return []
-    
+
 def main():
     st.sidebar.title("Navigation")
     app_mode = st.sidebar.radio("Choisissez l'onglet", ["Organisations engagées", "Localisation des Entreprises"])
--- organisations_engagees.py
+++ organisations_engagees.py
@@ -2,22 +2,6 @@ import streamlit as st
 import pandas as pd
 import requests
 
-def get_data():
-    url = "https://opendata.bordeaux-metropole.fr/api/records/1.0/search/?dataset=met_etablissement_rse&q=&rows=100"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Cela va déclencher une exception pour les réponses non-200
-        data = response.json()
-        records = data.get("records", [])
-        if records:
-            return [record.get("fields") for record in records]
-        else:
-            st.error("Aucun enregistrement trouvé dans les données de l'API.")
-            return []
-    except requests.RequestException as e:
-        st.error(f"Erreur lors de la récupération des données de l'API: {e}")
-        return []
-
 def display_organisations_engagees(data):
     st.markdown("## OPEN DATA RSE")
     st.markdown("### Découvrez les organisations engagées RSE de la métropole de Bordeaux")
