524960c0564ab36302a60a6977da7e222d5110c0
l.tripied <lt@bziiit.com>
2024-04-06 23:38:10 +0200

    V2-Liste_MAP_Stat_22

--- app.py
+++ app.py
@@ -15,5 +15,9 @@ def main():
     elif app_mode == "Statistiques":
         display_statistics()
 
+  # Après toutes les autres instructions dans votre barre latérale :
+    st.sidebar.markdown("---")  # Ajoute une ligne de séparation visuelle
+    st.sidebar.markdown("Powered by **bziiit IA RSE**")
+
 if __name__ == "__main__":
     main()
