2c9e4605670b30aaae771b80966c6250c4048043
l.tripied <lt@bziiit.com>
2024-04-06 13:59:57 +0200

    Pytorch V0.9

--- app.py
+++ app.py
@@ -9,8 +9,17 @@ def get_data():
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        records = data.get("records", [])
-        return [record.get("fields") for record in records]
+        records = data["records"]
+        data_for_display = []
+        for record in records:
+            field = record["fields"]
+            # Assume that 'geolocalisation' field is present and correctly formatted
+            if "geolocalisation" in field:
+                lat, lon = field["geolocalisation"]
+                field["latitude"] = lat
+                field["longitude"] = lon
+                data_for_display.append(field)
+        return data_for_display
     else:
         return []
 
@@ -19,25 +28,23 @@ def display_organisations_engagees(data):
     st.markdown("### Découvrez les organisations engagées RSE de la métropole de Bordeaux")
     
     df = pd.DataFrame(data)
-    if not df.empty:
-        df = df[["nom_courant_denomination", "commune", "libelle_section_naf", "tranche_effectif_entreprise", "action_rse"]]
-        st.dataframe(df.rename(columns={
-            "nom_courant_denomination": "Nom",
-            "commune": "Commune",
-            "libelle_section_naf": "Section NAF",
-            "tranche_effectif_entreprise": "Effectif",
-            "action_rse": "Action RSE"
-        }))
+    df = df[['nom_courant_denomination', 'commune', 'libelle_section_naf', 'tranche_effectif_entreprise', 'action_rse']]
+    st.dataframe(df.rename(columns={
+        'nom_courant_denomination': 'Nom',
+        'commune': 'Commune',
+        'libelle_section_naf': 'Section NAF',
+        'tranche_effectif_entreprise': 'Effectif',
+        'action_rse': 'Action RSE'
+    }))
 
 def display_map(data):
-    bordeaux_loc = [44.837789, -0.57918]
-    m = folium.Map(location=bordeaux_loc, zoom_start=12)
+    m = folium.Map(location=[44.837789, -0.57918], zoom_start=12)
     for item in data:
-        if item.get("geolocalisation"):
+        if 'latitude' in item and 'longitude' in item:
             folium.Marker(
-                location=[item["geolocalisation"][0], item["geolocalisation"][1]],
-                popup=item.get("nom_courant_denomination", "Information non disponible"),
+                [item['latitude'], item['longitude']],
                 icon=folium.Icon(color="green", icon="leaf"),
+                popup=item['nom_courant_denomination'],
             ).add_to(m)
     folium_static(m)
 
