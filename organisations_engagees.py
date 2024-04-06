b6be2f5ac76f408f16adb160c8c95555830d920c
l.tripied <lt@bziiit.com>
2024-04-06 15:12:34 +0200

    Pytorch V0.22

--- organisations_engagees.py
+++ organisations_engagees.py
@@ -5,8 +5,9 @@ def display_organisations_engagees(data):
     st.markdown("## OPEN DATA RSE")
     st.markdown("### Découvrez les organisations engagées RSE de la métropole de Bordeaux")
     num_etablissements = len(data)
-    st.markdown(f"Nombre d'établissements : {num_etablissements}")
-    if data:
+    
+    if num_etablissements > 0:
+        st.markdown(f"Nombre d'établissements : {num_etablissements}")
         df = pd.DataFrame(data)
         st.dataframe(df[['nom_courant_denomination', 'commune', 'libelle_section_naf', 'tranche_effectif_entreprise', 'action_rse']].rename(columns={
             'nom_courant_denomination': 'Nom',
@@ -15,3 +16,5 @@ def display_organisations_engagees(data):
             'tranche_effectif_entreprise': 'Effectif',
             'action_rse': 'Action RSE'
         }))
+    else:
+        st.error("Données OPEN DATA RSE Bordeaux non disponibles actuellement. Veuillez vous reconnecter ultérieurement.")
