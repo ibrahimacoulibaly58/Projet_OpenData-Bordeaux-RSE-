27cfa3cede397c193f37132c6255e3f73737e523
l.tripied <lt@bziiit.com>
2024-04-06 21:30:44 +0200

    V1-Liste_MAP_01

--- organisations_engagees.py
+++ organisations_engagees.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 from data_manager import get_data
@@ -8,8 +7,11 @@ def display_organisations_engagees():
     st.markdown("## OPEN DATA RSE")
     st.markdown("### Découvrez les organisations engagées RSE de Bordeaux Métropole")
     
-    data, _ = get_data()
+    data, total_hits = get_data()
     if data:
+        # Calcul du nombre total d'organisations
+        st.markdown(f"Nombre d'organisations : {total_hits}")
+        
         df = pd.DataFrame(data)
         df = df.rename(columns={
             "nom_courant_denomination": "Nom",
@@ -20,4 +22,3 @@ def display_organisations_engagees():
         })
         df = df[["Nom", "Commune", "Section NAF", "Effectif", "Action RSE"]]
         st.dataframe(df, width=None, height=None)
-
