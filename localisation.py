3f5c5c5686457b39cdf83e12d292fe8be90f9c6e
l.tripied <lt@bziiit.com>
2024-04-06 15:04:03 +0200

    Pytorch V0.19

--- localisation.py
+++ localisation.py
@@ -8,21 +8,28 @@ def get_data():
     if response.status_code == 200:
         data = response.json()
         records = data.get("records", [])
-        return [record.get("fields") for record in records]
+        cleaned_data = []
+        for record in records:
+            fields = record.get("fields", {})
+            # Assurez-vous que point_geo est un dictionnaire avec lat et lon.
+            point_geo = fields.get("geolocalisation")
+            if point_geo and isinstance(point_geo, list) and len(point_geo) == 2:
+                # Stockez directement lat et lon dans fields pour un accès facile.
+                fields["latitude"], fields["longitude"] = point_geo[0], point_geo[1]
+                cleaned_data.append(fields)
+        return cleaned_data
     else:
         return []
 
 def display_map(data):
     m = folium.Map(location=[44.837789, -0.57918], zoom_start=12)
     for item in data:
-        point_geo = item.get('point_geo')
-        if isinstance(point_geo, dict):
-            lon = point_geo.get('lon')
-            lat = point_geo.get('lat')
-            if lon and lat:
-                folium.Marker(
-                    [lat, lon],
-                    icon=folium.Icon(color="green", icon="leaf"),
-                    popup=item.get('nom_courant_denomination', 'Information non disponible'),
-                ).add_to(m)
+        # Utilisez directement latitude et longitude.
+        lat, lon = item.get("latitude"), item.get("longitude")
+        if lat and lon:
+            folium.Marker(
+                [lat, lon],
+                icon=folium.Icon(color="green", icon="leaf"),
+                popup=item.get('nom_courant_denomination', 'Information non disponible'),
+            ).add_to(m)
     folium_static(m)
