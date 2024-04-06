e383d262116bb022ffd30483af07e656aa6d2e34
l.tripied <lt@bziiit.com>
2024-04-06 21:14:02 +0200

    Pytorch v.51

--- app.py
+++ app.py
@@ -3,19 +3,9 @@ import pandas as pd
 import requests
 import folium
 from streamlit_folium import folium_static
+from data_manager import get_data
 from organisations_engagees import display_organisations_engagees
 from localisation import display_map
-
-# Fonction pour récupérer les données de l'API
-def get_data():
-    url = "https://opendata.bordeaux-metropole.fr/api/records/1.0/search/?dataset=met_etablissement_rse&q=&rows=100"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        records = data.get("records", [])
-        return [record["fields"] for record in records], data.get("nhits", 0)
-    else:
-        return [], 0
 
 # Main function orchestrating the app UI
 def main():
