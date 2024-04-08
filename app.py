9233c42636a6c4094b01a2931e8c25328cbde528
l.tripied <lt@bziiit.com>
2024-04-08 13:04:46 +0200

    Side barre 3 menus

--- app.py
+++ app.py
@@ -4,26 +4,53 @@ from localisation import display_map
 from statistiques import main as display_statistics
 from ActionsRSE import display_actions_rse
 
-# Main function orchestrating the app UI
+# Import des fonctions IA depuis le sous-répertoire
+# from IA_COMMERCE.Doc_Ref_COMMERCE import display_doc_ref_commerce
+# from IA_COMMERCE.BP_COMMERCE import display_bp_commerce
+# from IA_COMMERCE.Financement_TE_COMMERCE import display_Financement_TE_Commerce
+# from IA_COMMERCE.IA_COMMERCE import display_ia_commerce
+
 def main():
-    st.sidebar.title("Open Data RSE Bordeaux Métropole")
-    app_mode = st.sidebar.radio("Choisissez votre onglet", ["Organisations engagées", "Localisations", "Statistiques", "Actions RSE"])
+    st.sidebar.title("OPEN DATA & IA au service de la RSE")
+    section_principale = st.sidebar.radio(
+        "Choisissez votre section",
+        ["Data Bordeaux métropole", "Data bziiit", "IA RSE"]
+    )
 
-    if app_mode == "Organisations engagées":
-        display_organisations_engagees()
-    elif app_mode == "Localisations":
-        display_map()
-    elif app_mode == "Statistiques":
-        display_statistics()
-    elif app_mode == "Actions RSE":
-        display_actions_rse()
+    if section_principale == "Data Bordeaux métropole":
+        app_mode = st.sidebar.radio(
+            "Choisissez votre onglet",
+            ["Organisations engagées", "Localisations", "Statistiques", "Actions RSE"]
+        )
+        if app_mode == "Organisations engagées":
+            display_organisations_engagees()
+        elif app_mode == "Localisations":
+            display_map()
+        elif app_mode == "Statistiques":
+            display_statistics()
+        elif app_mode == "Actions RSE":
+            display_actions_rse()
 
-  # Après toutes les autres instructions dans votre barre latérale :
-    st.sidebar.markdown("---")  # Ajoute une ligne de séparation visuelle
+    elif section_principale == "Data bziiit":
+        ia_mode = st.sidebar.radio(
+            "Choisissez votre onglet",
+            ["Projets RSE", "Labels RSE", "Marques engagées"]
+        )
+#        if ia_mode == "Documents Référence":
+#            display_doc_ref_commerce()
+#        elif ia_mode == "Bonnes Pratiques":
+#            display_bp_commerce()
+#        elif ia_mode == "Financements":
+#            display_Financement_TE_Commerce()
+#        elif ia_mode == "Dialogue IA":
+#            display_ia_commerce()
+
+    # Instructions communes à toutes les sections
+    st.sidebar.markdown("---")
     st.sidebar.markdown("Powered by **bziiit IA RSE**")
-    st.sidebar.markdown("---")  # Ajoute une ligne de séparation visuelle
     st.sidebar.markdown("2024 : Open source en Licence MIT")
-    st.sidebar.markdown("---")  # Ajoute une ligne de séparation visuelle
+    st.sidebar.markdown("info@bziiit.com")
+    st.sidebar.markdown("---")
 
 if __name__ == "__main__":
     main()
